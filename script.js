b9a8c54bb8d321beed85dc2780779a8c01d7b11a
ArthurRomanha <126591597+ArthurRomanha@users.noreply.github.com>
2024-06-19 09:17:03 -0300

    a

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let gabarito = ["1A", "2C", "3A", "4A", "5A", "6A", "7B", "8A", "9A", "10A"];
 let minhasRespostas = [];
+let btnEnviar = document.getElementById("btnEnviar");
 
 document.addEventListener('submit', function(event){
     event.preventDefault();
+    btnEnviar.style.display = "none";
     for(let i = 0; i<10; i++){
     let minhaRespostaVez = document.querySelector(`input[name="opcoes${i+1}"]:checked`);
         minhasRespostas.push(minhaRespostaVez.value);
