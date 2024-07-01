ccd892e650222e314658d1b995bbe6c341f59ac8
ArthurRomanha <126591597+ArthurRomanha@users.noreply.github.com>
2024-07-01 16:58:34 -0300

    gabarito primeiras 45 questões prova azul 2023

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let gabarito = ["1A", "2C", "3A", "4A", "5A", "6A", "7B", "8A", "9A", "10A"];
+let gabarito = [
+    "1B", "2B", "3D", "4A", "5B", // valores existentes
+    "6D", "7A", "8C", "9E", "10E",
+    "11D", "12C", "13C", "14D", "15B",
+    "16A", "17D", "18B", "19D", "20E",
+    "21D", "22C", "23C", "24E", "25B",
+    "26C", "27A", "28C", "29E", "30A",
+    "31C", "32A", "33A", "34C", "35A",
+    "36A", "37C", "38A", "39C", "40B",
+    "41B", "42E", "43A", "44A", "45E"
+];
+
 let minhasRespostas = [];
 let btnEnviar = document.getElementById("btnEnviar");
 
